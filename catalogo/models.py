10da00b6826b6b0853a5aadc871e67393dfb7bc7
unknown <cristian>
2025-12-30 18:17:47 -0300

    modificaciones templates y migracion de modelo catalogo

--- catalogo/models.py
+++ catalogo/models.py
@@ -1,10 +1,24 @@
 from django.db import models
 
 # Create your models here.
+# class Juego(models.Model):
+#     nombre=models.CharField(max_length=100)
+#     precio=models.DecimalField(max_digits=6,decimal_places=2)
+#     plataforma=models.CharField(max_length=200)
+#     imagen=models.CharField(max_length=200,default="default.jpg")
+#     def __str__(self):
+#         return self.nombre
+
 class Juego(models.Model):
-    nombre=models.CharField(max_length=100)
-    precio=models.DecimalField(max_digits=6,decimal_places=2)
-    plataforma=models.CharField(max_length=200)
-    imagen=models.CharField(max_length=200,default="default.jpg")
+    nombre = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+    plataforma = models.CharField(max_length=200)
+
+    imagen = models.ImageField(
+        upload_to='juegos/',
+        blank=True,
+        null=True
+    )
+
     def __str__(self):
         return self.nombre
